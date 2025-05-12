0dd0143210a6332c6d378c1a8c1a1960dc03fe68
CodingCatAero <hamsjeryne@proton.me>
2025-05-12 16:41:31 +1000

    Fixed filename bug and added lower bound for bitrate

--- 8mb.py
+++ 8mb.py
@@ -11,31 +11,10 @@ startTime = time.time()
 # Parses args from cli command
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help='File to crush', required=True)
-parser.add_argument("-s", "--size", help="Target Size in MiB (1024 KiB)", type=int, required=True)
-parser.add_argument("-t", "--tolerance", help="Tolerance in % (default: 5)", type=int, required=True)
+parser.add_argument("-s", "--size", help="Target Size in MiB (1024 KiB)", type=int, default=8, required=False)
+parser.add_argument("-t", "--tolerance", help="Tolerance in % (default: 5)", type=int, default=5, required=False)
 parser.add_argument('-o', '--output', help='Name for output filename (default: <file>.crushed.<file type>)', required=False)
 args = parser.parse_args()
-
-def transcode(fileInput, fileOutput, bitrate):
-    command = [
-        'ffmpeg',
-            '-y',
-            '-hide_banner',
-            '-loglevel', 'error',
-            '-i', fileInput,
-            '-b', str(bitrate) + '',
-            '-cpu-used', str(os.cpu_count()),
-            '-c:a',
-            'copy',
-            fileOutput
-      ]
-
-    proc = subprocess.run(
-                command,
-                capture_output=True,
-                # avoid having to explicitly encode
-                text=True
-    )
 
 # Returns a string which contains the correct size factor for the amount of bytes thrown in
 def determineByteSize(bytes, perSecond=0):
@@ -61,34 +40,38 @@ fileInput = args.file
 if args.output:
     fileOutput = args.output
 else:
-    fileOutput = fileInput[:fileInput.rindex('.')] + '.crushed' + fileInput[fileInput.rindex('.'):]
+    fileOutput = f"{fileInput[:fileInput.rindex('.')]}_CRUSHED_{fileInput[fileInput.rindex('.'):]}"
 
 targetSizeBytes = (args.size * 1024) * 1024
 
 # Should get the duration of the video as a float by running a subprocess, reading the stdout, and turning it into a float
-durationSeconds = float((
-    subprocess.run(f"ffprobe -i \"{fileInput}\" -show_entries format=duration -v quiet -of csv=\"p=0\"", shell=True, text=True, capture_output=True)
-    ).stdout)
+durationSeconds = float((subprocess.run(f"ffprobe -i \"{fileInput}\" -show_entries format=duration -v quiet -of csv=\"p=0\"", 
+                  shell=True, text=True, capture_output=True)).stdout)
 
 # Rough calculation of bitrate by dividing target bytes by duration of video
 bitrate = targetSizeBytes / durationSeconds
 
 print(f"Crushing {fileInput} to {determineByteSize(targetSizeBytes)} with {tolerance}% tolerance.", end="\n\n")
 
-factor = 0
+factor = None
 attempt = 0
 
 # Should loop while the video is larger or smaller than the target size +/- the tolerance %
-while (factor > 1.0 + (tolerance/100.0)) or (factor < 1.0 - (tolerance/100.0)):
+while (factor == None or factor > 1.0 + (tolerance/100.0) or factor < 1.0 - (tolerance/100.0)) and bitrate > 5000:
     attempt += 1
 
     # Sets the target bitrate by taking the existing bitrate and multiplying it by 1 (On initial loop), or by the factor of size which the video is larger
-    bitrate = bitrate * (factor or 1)
+    bitrate = round(bitrate * (factor or 1))
+
+    if bitrate < 5000:
+        print("BITRATE TOO LOW. Setting to 5KiB/s and terminating crushing after this transcode.", end="\n\n")
+        bitrate = 5000
 
     print(f"Attempt {attempt}: Transcoding {fileInput} at {determineByteSize(bitrate, perSecond=1)}")
 
     # Transcodes using the above function
-    transcode(fileInput, fileOutput, round(bitrate))
+    subprocess.run(f"ffmpeg -y -hide_banner -loglevel error -i \"{fileInput}\" -b:v {str(bitrate)} -cpu-used {str(os.cpu_count())} -c:a copy \"{fileOutput}\"",
+    shell=True, text=True)
 
     # Gets before and after sizes
     beforeSizeBytes = os.stat(fileInput).st_size
